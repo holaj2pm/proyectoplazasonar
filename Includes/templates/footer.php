96542bea10abb1ade8b53f6fed97cb5e6c2d5e13
holaj2pm <137113824+holaj2pm@users.noreply.github.com>
2023-11-23 06:04:58 -0400

    Add files via upload

---
+++ Includes/templates/footer.php
@@ -0,0 +1,9 @@
+<!-- INCLUDE JS SCRIPTS -->
+<script src="Design/js/jquery.min.js"></script>
+<script src="Design/js/bootstrap.min.js"></script>
+<script src="Design/js/bootstrap.bundle.min.js"></script>
+<script src="Design/js/main.js"></script>
+
+</body>
+
+</html>
